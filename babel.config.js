6f9d8d3c7f1a8559a10f8d4301b19eb9127c559c
tmkttmkt <24rd123@ms.dendai.ac.jp>
2025-04-17 22:39:13 +0900

    どうして...

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,17 @@
-// filepath: c:\TK\github\HackathonApp1\babel.config.js
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ['module:react-native-dotenv', {
-      moduleName: '@env',
-      path: '.env',
-      blacklist: null,
-      whitelist: null,
-      safe: false,
-      allowUndefined: true,
-    }],
-    ['@babel/plugin-transform-private-methods', { loose: true }], // プライベートメソッドの変換プラグイン
-    ['@babel/plugin-transform-class-properties', { loose: true }], // クラスプロパティの変換プラグイン
-    ['@babel/plugin-transform-private-property-in-object', { loose: true }], // プライベートプロパティの変換プラグイン
-  ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      'babel-preset-expo',
+      ['@babel/preset-react', { runtime: 'automatic' }]
+    ],
+    plugins: [
+      ['module:react-native-dotenv', {
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+      }],
+    ],
+  };
 };

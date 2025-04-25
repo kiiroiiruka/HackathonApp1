0d9f02dc0d4113ca1594c15a14629c96efccafde
tmkttmkt <24rd123@ms.dendai.ac.jp>
2025-04-26 00:01:07 +0900

    k

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,42 @@
-
 module.exports = [
-  {
-    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-      parser: '@typescript-eslint/parser',
-      globals: {
-        browser: true,
-        node: true,
-      },
+    {
+      ignores: ['dist'], // 無視するファイル・ディレクトリ
     },
-    plugins: {
-      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    {
+        files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: 'module',
+            parser: '@typescript-eslint/parser',
+            globals: {
+                browser: true,
+                node: true,
+            },
+        },
+        plugins: {
+            '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+            prettier: require('eslint-plugin-prettier'),
+            react: require('eslint-plugin-react'),
+            'react-native': require('eslint-plugin-react-native'),
+        },
+        extends: [
+          'eslint:recommended',
+          'plugin:react/recommended',
+          'plugin:react-native/all',
+          'prettier',
+        ],
+        settings: {
+            react: {
+                version: 'detect', // Reactバージョンを自動検出
+            },
+        },
+        rules: {
+            'prefer-const': 'error',
+            'consistent-return': 'error',
+            'prefer-template': 'error',
+            'no-multiple-empty-lines': ['error', { max: 2 }],
+            'no-mixed-spaces-and-tabs': 'error',
+            'prettier/prettier': 'error', // Prettierルールを適用
+        },
     },
-    rules: {
-      // カスタムルールをここに追加
-      'no-unused-vars': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn',
-    },
-  },
 ];

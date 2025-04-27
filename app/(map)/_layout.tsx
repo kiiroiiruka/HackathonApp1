85ea38c71d595d93e03272ba12454f711d225dcd
Masa0659 <24rd164@ms.dendai.ac.jp>
2025-04-27 13:59:08 +0900

    web版のアイコンと名前の変更

---
+++ app/(map)/_layout.tsx
@@ -0,0 +1,17 @@
+import { Stack } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+const Layout = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      {/* Stackでラップしているのでページ遷移はここから行います */}
+      <Stack>
+        {/* 他のページへのリンクが必要なら、Stack.Screenで定義できます */}
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="index.web" options={{ headerShown: false }} />
+      </Stack>
+    </View>
+  );
+};
+
+export default Layout;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -62,6 +62,7 @@ const Layout = () => {
           <Stack.Screen name="(login)" options={{ headerShown: false }} />
           <Stack.Screen name="(main)" options={{ headerShown: false }} />
           <Stack.Screen name="(chat)" options={{ headerShown: false }} />
+          <Stack.Screen name="(map)" options={{ headerShown: false }} />
         </Stack>
       </View>
     </SafeAreaView>

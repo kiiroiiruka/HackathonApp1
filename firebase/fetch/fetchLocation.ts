9842ad86e47e76245b54c16a5e7325bbbeb6a953
TMKT <126658967+tmkttmkt@users.noreply.github.com>
2025-04-27 13:28:47 +0900

    vesubh

--- firebase/fetch/fetchLocation.ts
+++ firebase/fetch/fetchLocation.ts
@@ -5,6 +5,9 @@ export const updateLocation = async function (
   userKey: string,
   location: Location.LocationObject,
 ) {
+  if(!userKey) {
+    console.warn("無効なユーザーキーが提供されました。位置情報の更新をスキップします。");    return;
+  }
   const coords = location.coords;
   const locationRef = ref(realtimeDb, `locations/${userKey}`);
   await set(locationRef, coords);
